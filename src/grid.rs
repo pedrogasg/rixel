403f4200d7d16476635378e26686f6b049629fe2
Pedro Fillastre <pedrogasg_g@hotmail.com>
2022-12-10 17:40:25 +0100

    Adding grid

--- src/grid.rs
+++ src/grid.rs
@@ -19,10 +19,7 @@ impl Default for GridPlugin {
 }
 impl GridPlugin {
     pub fn new(width: u32, height: u32) -> Self {
-        Self {
-            width,
-            height,
-        }
+        Self { width, height }
     }
 }
 
@@ -45,18 +42,30 @@ struct GridSize {
 }
 
 fn spawn_cells(
+    windows: Res<Windows>,
     grid_size: ResMut<GridSize>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<cell::CellMaterial>>,
 ) {
-    let size = 100.;
-
+    let (width,heigth) = match windows.get_primary() {
+        Some(window) =>  (window.width(), window.height()),
+        None => (1024., 1024.)
+    };
+    let size = width / grid_size.width as f32;
+    let left = (width / 2.) - (size / 2.);
+    let top = (heigth / 2.) - (size / 2.);
     for (i, j) in iproduct!(0..grid_size.width, 0..grid_size.height) {
+        let color = if i % 2 == 0 {
+            Color::ALICE_BLUE
+        } else {
+            Color::DARK_GRAY
+        };
+
         commands.spawn(MaterialMesh2dBundle {
             mesh: meshes.add(cell::Cell::new(size).into()).into(),
-            material: materials.add(cell::CellMaterial::new(Color::ALICE_BLUE)),
-            transform: Transform::from_xyz(size * i as f32, size * j as f32, 0.),
+            material: materials.add(cell::CellMaterial::new(color)),
+            transform: Transform::from_xyz((size * i as f32) - left, (size * j as f32) - top, 0.),
             ..default()
         });
     }
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,44 @@ use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
 mod cell;
 mod grid;
+
+pub const HEIGHT: f32 = 1280.0;
+pub const WIDTH: f32 = 1280.0;
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            watch_for_changes: true,
-            ..default()
-        }))
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    watch_for_changes: true,
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        width: WIDTH,
+                        height: HEIGHT,
+                        title: "Rixel".to_string(),
+                        resizable: false,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                }),
+        )
         .add_startup_system(setup)
-        .add_plugin(grid::GridPlugin::new(3,3))
-        .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(grid::GridPlugin::new(30, 30))
+        //.add_plugin(WorldInspectorPlugin::new())
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-) {
-    commands.spawn(Camera2dBundle::default());
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle{
+        projection: OrthographicProjection{
+            left: -(WIDTH / 2.),
+            right: WIDTH / 2.,
+            top: -(HEIGHT / 2.),
+            bottom: HEIGHT / 2.,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
